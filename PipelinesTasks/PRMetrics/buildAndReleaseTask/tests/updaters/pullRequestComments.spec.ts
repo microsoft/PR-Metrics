e0556764e2ebeba0f2e76adc955b9278aac3c91c
Muiris Woulfe <mwoulfe@microsoft.com>
2021-03-22 16:11:02 +0000

    Updating code

--- PipelinesTasks/PRMetrics/buildAndReleaseTask/tests/updaters/pullRequestComments.spec.ts
+++ PipelinesTasks/PRMetrics/buildAndReleaseTask/tests/updaters/pullRequestComments.spec.ts
@@ -1,8 +1,11 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+import async from 'async'
+import os from 'os'
 import PullRequestComments from '../../updaters/pullRequestComments'
 import TaskLibWrapper from '../../wrappers/taskLibWrapper'
+import { CommentThreadStatus } from 'azure-devops-node-api/interfaces/GitInterfaces'
 import { expect } from 'chai'
 import { instance, mock, verify, when } from 'ts-mockito'
 
@@ -11,7 +14,19 @@ describe('pullRequestComments.ts', (): void => {
 
   beforeEach((): void => {
     taskLibWrapper = mock(TaskLibWrapper)
+    when(taskLibWrapper.loc('updaters.pullRequestComments.commentFooter')).thenReturn('[Metrics added by PR Metrics. Add to Azure DevOps today!](https://marketplace.visualstudio.com/items?itemName=ms-omex.prmetrics)')
+    when(taskLibWrapper.loc('updaters.pullRequestComments.commentTitle', '1')).thenReturn('# Metrics for iteration 1')
     when(taskLibWrapper.loc('updaters.pullRequestComments.fileIgnoredComment')).thenReturn('❗ **This file may not need to be reviewed.**')
+    when(taskLibWrapper.loc('updaters.pullRequestComments.largePullRequestComment', '1,000')).thenReturn('❌ **Try to keep pull requests smaller than 1,000 lines of new product code by following the [Single Responsibility Principle (SRP)](https://wikipedia.org/wiki/Single-responsibility_principle).**')
+    when(taskLibWrapper.loc('updaters.pullRequestComments.smallPullRequestComment')).thenReturn('✔ **Thanks for keeping your pull request small.**')
+    when(taskLibWrapper.loc('updaters.pullRequestComments.tableIgnored')).thenReturn('Ignored')
+    when(taskLibWrapper.loc('updaters.pullRequestComments.tableLines')).thenReturn('Lines')
+    when(taskLibWrapper.loc('updaters.pullRequestComments.tableProductCode')).thenReturn('Product Code')
+    when(taskLibWrapper.loc('updaters.pullRequestComments.tableSubtotal')).thenReturn('Subtotal')
+    when(taskLibWrapper.loc('updaters.pullRequestComments.tableTestCode')).thenReturn('Test Code')
+    when(taskLibWrapper.loc('updaters.pullRequestComments.tableTotal')).thenReturn('Total')
+    when(taskLibWrapper.loc('updaters.pullRequestComments.testsInsufficientComment')).thenReturn('⚠️ **Consider adding additional tests.**')
+    when(taskLibWrapper.loc('updaters.pullRequestComments.testsSufficientComment')).thenReturn('✔ **Thanks for adding tests.**')
   })
 
   describe('getCommentData()', (): void => {
@@ -51,23 +66,59 @@ describe('pullRequestComments.ts', (): void => {
       const result: string = pullRequestComments.getMetricsComment()
 
       // Assert
-      expect(result).to.equal('TODO')
+      expect(result).to.equal(
+        `# Metrics for iteration 1${os.EOL}` +
+        `✔ **Thanks for keeping your pull request small.**${os.EOL}` +
+        `✔ **Thanks for adding tests.**${os.EOL}` +
+        `||Lines${os.EOL}` +
+        `-|-:${os.EOL}` +
+        `Product Code|1,000${os.EOL}` +
+        `Test Code|1,000${os.EOL}` +
+        `**Subtotal**|**1,000**${os.EOL}` +
+        `Ignored|1,000${os.EOL}` +
+        `**Total**|**1,000**${os.EOL}` +
+        os.EOL +
+        '[Metrics added by PR Metrics. Add to Azure DevOps today!](https://marketplace.visualstudio.com/items?itemName=ms-omex.prmetrics)')
       verify(taskLibWrapper.debug('* PullRequestComments.getMetricsComment()')).once()
+      verify(taskLibWrapper.debug('* PullRequestComments.addCommentSizeStatus()')).once()
+      verify(taskLibWrapper.debug('* PullRequestComments.addCommentTestStatus()')).once()
+      verify(taskLibWrapper.debug('* PullRequestComments.addCommentMetrics()')).times(5)
     })
   })
 
   describe('getMetricsCommentStatus()', (): void => {
-    it('should return the expected result', (): void => {
+    it('should return Closed when the pull request is small and has sufficient test coverage', (): void => {
       // Arrange
       const pullRequestComments: PullRequestComments = new PullRequestComments(instance(taskLibWrapper))
 
       // Act
-      const result: string = pullRequestComments.getMetricsCommentStatus()
+      const result: CommentThreadStatus = pullRequestComments.getMetricsCommentStatus(true, true)
 
       // Assert
-      expect(result).to.equal('TODO')
+      expect(result).to.equal(CommentThreadStatus.Closed)
       verify(taskLibWrapper.debug('* PullRequestComments.getMetricsCommentStatus()')).once()
     })
+
+    async.each(
+      [
+        [true, false],
+        [true, null],
+        [false, true],
+        [false, false],
+        [false, null]
+      ], (parameters: (boolean| null)[]): void => {
+        it(`should return Active when the pull request small status is '${parameters[0]}' and the sufficient test coverage status is '${parameters[1]}'`, (): void => {
+          // Arrange
+          const pullRequestComments: PullRequestComments = new PullRequestComments(instance(taskLibWrapper))
+
+          // Act
+          const result: CommentThreadStatus = pullRequestComments.getMetricsCommentStatus(parameters[0]!, parameters[1]!)
+
+          // Assert
+          expect(result).to.equal(CommentThreadStatus.Active)
+          verify(taskLibWrapper.debug('* PullRequestComments.getMetricsCommentStatus()')).once()
+        })
+      })
   })
 
   describe('getIgnoredComment()', (): void => {
--- PipelinesTasks/PRMetrics/buildAndReleaseTask/updaters/pullRequestComments.ts
+++ PipelinesTasks/PRMetrics/buildAndReleaseTask/updaters/pullRequestComments.ts
@@ -1,7 +1,9 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+import * as os from 'os'
 import TaskLibWrapper from '../wrappers/taskLibWrapper'
+import { CommentThreadStatus } from 'azure-devops-node-api/interfaces/GitInterfaces'
 
 /**
  * A class for managing pull requests comments.
@@ -44,17 +46,39 @@ class PullRequestComments {
   public getMetricsComment (): string {
     this.taskLibWrapper.debug('* PullRequestComments.getMetricsComment()')
 
-    return 'TODO' // TODO: Update once dependencies are added
+    // TODO: Update once dependencies are added
+    let result: string = `${this.taskLibWrapper.loc('updaters.pullRequestComments.commentTitle', '1')}${os.EOL}`
+    result += this.addCommentSizeStatus(1000, true)
+    result += this.addCommentTestStatus(true)
+
+    result += `||${this.taskLibWrapper.loc('updaters.pullRequestComments.tableLines')}${os.EOL}`
+    result += `-|-:${os.EOL}`
+    result += this.addCommentMetrics(this.taskLibWrapper.loc('updaters.pullRequestComments.tableProductCode'), 1000, false)
+    result += this.addCommentMetrics(this.taskLibWrapper.loc('updaters.pullRequestComments.tableTestCode'), 1000, false)
+    result += this.addCommentMetrics(this.taskLibWrapper.loc('updaters.pullRequestComments.tableSubtotal'), 1000, true)
+    result += this.addCommentMetrics(this.taskLibWrapper.loc('updaters.pullRequestComments.tableIgnored'), 1000, false)
+    result += this.addCommentMetrics(this.taskLibWrapper.loc('updaters.pullRequestComments.tableTotal'), 1000, true)
+
+    result += os.EOL
+    result += this.taskLibWrapper.loc('updaters.pullRequestComments.commentFooter')
+
+    return result
   }
 
   /**
    * Gets the status to which to update the comment thread.
+   * @param isSmall A value indicating whether the pull request is small or extra small.
+   * @param hasSufficientTestCode A value indicating whether the pull request has sufficient test code.
    * @returns The status to which to update the comment thread.
    */
-  public getMetricsCommentStatus (): string {
+  public getMetricsCommentStatus (isSmall: boolean, hasSufficientTestCode: boolean | null): CommentThreadStatus {
     this.taskLibWrapper.debug('* PullRequestComments.getMetricsCommentStatus()')
 
-    return 'TODO' // TODO: Update once dependencies are added
+    if (isSmall && hasSufficientTestCode) {
+      return CommentThreadStatus.Closed
+    }
+
+    return CommentThreadStatus.Active
   }
 
   /**
@@ -65,6 +89,48 @@ class PullRequestComments {
     this.taskLibWrapper.debug('* PullRequestComments.getIgnoredComment()')
 
     return this.taskLibWrapper.loc('updaters.pullRequestComments.fileIgnoredComment')
+  }
+
+  private addCommentSizeStatus (baseSize: number, isSmall: boolean): string {
+    this.taskLibWrapper.debug('* PullRequestComments.addCommentSizeStatus()')
+
+    let result: string = ''
+    if (isSmall) {
+      result += this.taskLibWrapper.loc('updaters.pullRequestComments.smallPullRequestComment')
+    } else {
+      result += this.taskLibWrapper.loc('updaters.pullRequestComments.largePullRequestComment', baseSize.toLocaleString())
+    }
+
+    result += os.EOL
+    return result
+  }
+
+  private addCommentTestStatus (hasSufficientTestCode: boolean | null): string {
+    this.taskLibWrapper.debug('* PullRequestComments.addCommentTestStatus()')
+
+    let result: string = ''
+    if (hasSufficientTestCode !== null) {
+      if (hasSufficientTestCode) {
+        result += this.taskLibWrapper.loc('updaters.pullRequestComments.testsSufficientComment')
+      } else {
+        result += this.taskLibWrapper.loc('updaters.pullRequestComments.testsInsufficientComment')
+      }
+
+      result += os.EOL
+    }
+
+    return result
+  }
+
+  private addCommentMetrics (title: string, metric: number, highlight: boolean): string {
+    this.taskLibWrapper.debug('* PullRequestComments.addCommentMetrics()')
+
+    let surround: string = ''
+    if (highlight) {
+      surround = '**'
+    }
+
+    return `${surround}${title}${surround}|${surround}${metric.toLocaleString()}${surround}${os.EOL}`
   }
 }
 
