9fc15dbd9eb8ebca84a468df9b70f3cad2858837
Muiris Woulfe <mwoulfe@microsoft.com>
2021-03-22 11:06:12 +0000

    Adding additional tests

--- PipelinesTasks/PRMetrics/buildAndReleaseTask/tests/updaters/pullRequest.spec.ts
+++ PipelinesTasks/PRMetrics/buildAndReleaseTask/tests/updaters/pullRequest.spec.ts
@@ -43,4 +43,44 @@ describe('pullRequest.ts', (): void => {
       verify(taskLibWrapper.debug('* PullRequest.isPullRequest()')).once()
     })
   })
+
+  describe('getUpdatedDescription()', (): void => {
+    it('should return null when the current description is set', (): void => {
+      // Arrange
+      const pullRequest: PullRequest = new PullRequest(instance(taskLibWrapper))
+
+      // Act
+      const result: string | null = pullRequest.getUpdatedDescription('Description')
+
+      // Assert
+      expect(result).to.equal(null)
+      verify(taskLibWrapper.debug('* PullRequest.getUpdatedDescription()')).once()
+    })
+
+    it('should return the default description when the current description is empty', (): void => {
+      // Arrange
+      const pullRequest: PullRequest = new PullRequest(instance(taskLibWrapper))
+
+      // Act
+      const result: string | null = pullRequest.getUpdatedDescription('')
+
+      // Assert
+      expect(result).to.equal('❌ **Add a description.**')
+      verify(taskLibWrapper.debug('* PullRequest.getUpdatedDescription()')).once()
+    })
+  })
+
+  describe('getUpdatedTitle()', (): void => {
+    it('should return null when the current title is set to the expected title', (): void => {
+      // Arrange
+      const pullRequest: PullRequest = new PullRequest(instance(taskLibWrapper))
+
+      // Act
+      const result: string | null = pullRequest.getUpdatedTitle('S✔ ◾ Title', 'S✔')
+
+      // Assert
+      expect(result).to.equal(null)
+      verify(taskLibWrapper.debug('* PullRequest.getUpdatedTitle()')).once()
+    })
+  })
 })
