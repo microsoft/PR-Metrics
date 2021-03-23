903a45cf3d9d8d6f07b23b2a7148298ffe183dae
Aubrey Quinn <80953505+aubreyquinn@users.noreply.github.com>
2021-03-23 11:26:08 +0000

    Pr code metrics (#27)
    I converted the main functionality into TypeScript.

---
+++ PipelinesTasks/PRMetrics/buildAndReleaseTask/updaters/iMetrics.ts
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+export interface IMetrics {
+    productCode: number;
+    testCode: number;
+    subtotal: number;
+    ignored: number;
+    total: number;
+}
