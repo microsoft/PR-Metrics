b93ad041d3e1a85997fbdedf904b02541aec59d9
Muiris Woulfe <mwoulfe@microsoft.com>
2021-03-23 12:00:57 +0000

    Updating process wrapper casing

--- PipelinesTasks/PRMetrics/buildAndReleaseTask/wrappers/ProcessWrapper.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-class ProcessWrapper {
-  public write (input: string): void {
-    process.stdout.write(input)
-  }
-}
-
-export default ProcessWrapper
