a29c0b80f83247ad0eaa104dae1cca32e04d8f14
Muiris Woulfe <mwoulfe@microsoft.com>
2021-03-24 19:56:09 +0000

    Updating validator

--- PipelinesTasks/PRMetrics/buildAndReleaseTask/tests/utilities/validator.spec.ts
+++ PipelinesTasks/PRMetrics/buildAndReleaseTask/tests/utilities/validator.spec.ts
@@ -38,6 +38,34 @@ describe('validator.ts', (): void => {
         })
       })
 
+    async.each(
+      [
+        null,
+        undefined
+      ], (value: string[] | null | undefined): void => {
+        it(`should throw an error when passed invalid string array value '${value}'`, (): void => {
+          // Act
+          const func: () => void = () => validator.validateField(value, 'string array test')
+
+          // Assert
+          expect(func).to.throw(`Field 'string array test' is invalid, null, or undefined '${value}'.`)
+        })
+      })
+
+    async.each(
+      [
+        null,
+        undefined
+      ], (value: object | null | undefined): void => {
+        it(`should throw an error when passed invalid object value '${value}'`, (): void => {
+          // Act
+          const func: () => void = () => validator.validateField(value, 'object test')
+
+          // Assert
+          expect(func).to.throw(`Field 'object test' is invalid, null, or undefined '${value}'.`)
+        })
+      })
+
     it('should not throw an error when passed a valid number value', (): void => {
       // Act
       const func: () => void = () => validator.validateField(1, 'number test')
@@ -49,6 +77,22 @@ describe('validator.ts', (): void => {
     it('should not throw an error when passed a valid string value', (): void => {
       // Act
       const func: () => void = () => validator.validateField('value', 'string test')
+
+      // Assert
+      expect(func).not.to.throw()
+    })
+
+    it('should not throw an error when passed a valid string array value', (): void => {
+      // Act
+      const func: () => void = () => validator.validateField([], 'string array test')
+
+      // Assert
+      expect(func).not.to.throw()
+    })
+
+    it('should not throw an error when passed a valid object value', (): void => {
+      // Act
+      const func: () => void = () => validator.validateField({}, 'object test')
 
       // Assert
       expect(func).not.to.throw()
