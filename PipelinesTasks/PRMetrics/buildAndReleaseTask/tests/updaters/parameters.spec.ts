7599d73b56d573c6e9ff18896e4aab5380a8d2a6
Aubrey Quinn <aubreyquinn@microsoft.com>
2021-03-24 12:00:24 +0000

    parameters unit tests in progress

--- PipelinesTasks/PRMetrics/buildAndReleaseTask/tests/updaters/parameters.spec.ts
+++ PipelinesTasks/PRMetrics/buildAndReleaseTask/tests/updaters/parameters.spec.ts
@@ -18,287 +18,310 @@ describe('parameters.ts', (): void => {
     consoleWrapper = mock(ConsoleWrapper)
   })
   describe('initialize', (): void => {
-    async.each(
-      [
-        '',
-        ' ',
-        'abc',
-        '===',
-        '!2',
-        'null',
-        'undefined'
-      ], (currentBaseSize: string): void => {
-        it(`initializeBaseSize - should give a value of 250 when base size is invalid input '${currentBaseSize}'`, (): void => {
+    describe('initializeBaseSize', (): void => {
+      async.each(
+        [
+          '',
+          ' ',
+          'abc',
+          '===',
+          '!2',
+          'null',
+          'undefined'
+        ], (currentBaseSize: string): void => {
+          it(`initializeBaseSize - should give a value of 250 when base size is invalid input '${currentBaseSize}'`, (): void => {
           // Arrange
 
-          const parameters: Parameters = new Parameters(instance(consoleWrapper), instance(taskLibWrapper))
+            const parameters: Parameters = new Parameters(instance(consoleWrapper), instance(taskLibWrapper))
 
-          // Act
-          parameters.initialize(currentBaseSize, '', '', '', '') // NaN
+            // Act
+            parameters.initialize(currentBaseSize, '', '', '', '') // NaN
 
-          // Assert
-          expect(parameters.baseSize).to.equal(250)
+            // Assert
+            expect(parameters.baseSize).to.equal(250)
+          })
         })
-      })
 
-    async.each(
-      [
-        '0',
-        '-1',
-        '-1000',
-        '-5'
-      ], (currentBaseSize: string): void => {
-        it(`initializeBaseSize - should give a value of 250 when base size is less than or equal to 0 '${currentBaseSize}'`, (): void => {
+      async.each(
+        [
+          '0',
+          '-1',
+          '-1000',
+          '-5'
+        ], (currentBaseSize: string): void => {
+          it(`initializeBaseSize - should give a value of 250 when base size is less than or equal to 0 '${currentBaseSize}'`, (): void => {
           // Arrange
 
-          const parameters: Parameters = new Parameters(instance(consoleWrapper), instance(taskLibWrapper))
+            const parameters: Parameters = new Parameters(instance(consoleWrapper), instance(taskLibWrapper))
 
-          // Act
-          parameters.initialize(currentBaseSize, '', '', '', '') // negative or zero
+            // Act
+            parameters.initialize(currentBaseSize, '', '', '', '') // negative or zero
 
-          // Assert
-          expect(parameters.baseSize).to.equal(250)
+            // Assert
+            expect(parameters.baseSize).to.equal(250)
+          })
         })
-      })
 
-    async.each(
-      [
-        '1',
-        '5',
-        '1000',
-        '5.5'
-      ], (currentBaseSize: string): void => {
-        it(`initializeBaseSize - should give the converted value when base size is greater than 0 '${currentBaseSize}'`, (): void => {
+      async.each(
+        [
+          '1',
+          '5',
+          '1000',
+          '5.5'
+        ], (currentBaseSize: string): void => {
+          it(`initializeBaseSize - should give the converted value when base size is greater than 0 '${currentBaseSize}'`, (): void => {
           // Arrange
 
-          const parameters: Parameters = new Parameters(instance(consoleWrapper), instance(taskLibWrapper))
+            const parameters: Parameters = new Parameters(instance(consoleWrapper), instance(taskLibWrapper))
 
-          // Act
-          parameters.initialize(currentBaseSize, '', '', '', '') // positive integer
+            // Act
+            parameters.initialize(currentBaseSize, '', '', '', '') // positive integer
 
-          // Assert
-          expect(parameters.baseSize).to.equal(parseInt(currentBaseSize))
+            // Assert
+            expect(parameters.baseSize).to.equal(parseInt(currentBaseSize))
+          })
         })
-      })
+    })
 
-    /** initializeGrowthRate */
-    async.each(
-      [
-        '',
-        ' ',
-        'abc',
-        '===',
-        '!2',
-        'null',
-        'undefined'
-      ], (currentGrowthRate: string): void => {
-        it(`initializeGrowthRate - should give a value of 2.0 when base size is invalid input '${currentGrowthRate}'`, (): void => {
+    describe('initializeGrowthRate', (): void => {
+      async.each(
+        [
+          '',
+          ' ',
+          'abc',
+          '===',
+          '!2',
+          'null',
+          'undefined'
+        ], (currentGrowthRate: string): void => {
+          it(`initializeGrowthRate - should give a value of 2.0 when base size is invalid input '${currentGrowthRate}'`, (): void => {
           // Arrange
 
-          const parameters: Parameters = new Parameters(instance(consoleWrapper), instance(taskLibWrapper))
+            const parameters: Parameters = new Parameters(instance(consoleWrapper), instance(taskLibWrapper))
 
-          // Act
-          parameters.initialize('', currentGrowthRate, '', '', '') // NaN
+            // Act
+            parameters.initialize('', currentGrowthRate, '', '', '') // NaN
 
-          // Assert
-          expect(parameters.growthRate).to.equal(2.0)
+            // Assert
+            expect(parameters.growthRate).to.equal(2.0)
+          })
         })
-      })
 
-    async.each(
-      [
-        '0',
-        '0.5',
-        '-2',
-        '-1.2',
-        '-5',
-        '0.9999999999'
-      ], (currentGrowthRate: string): void => {
-        it(`initializeGrowthRate - should give a value of 2.0 when converted value is less than 1.0 '${currentGrowthRate}'`, (): void => {
+      async.each(
+        [
+          '0',
+          '0.5',
+          '-2',
+          '-1.2',
+          '-5',
+          '0.9999999999'
+        ], (currentGrowthRate: string): void => {
+          it(`initializeGrowthRate - should give a value of 2.0 when converted value is less than 1.0 '${currentGrowthRate}'`, (): void => {
           // Arrange
 
-          const parameters: Parameters = new Parameters(instance(consoleWrapper), instance(taskLibWrapper))
+            const parameters: Parameters = new Parameters(instance(consoleWrapper), instance(taskLibWrapper))
 
-          // Act
-          parameters.initialize('', currentGrowthRate, '', '', '') // < 1.0
+            // Act
+            parameters.initialize('', currentGrowthRate, '', '', '') // < 1.0
 
-          // Assert
-          expect(parameters.growthRate).to.equal(2.0)
+            // Assert
+            expect(parameters.growthRate).to.equal(2.0)
+          })
         })
-      })
 
-    async.each(
-      [
-        '5',
-        '2.0',
-        '1000',
-        '1.001',
-        '1.2',
-        '1.0000000001',
-        '1.09',
-        '7'
-      ], (currentGrowthRate: string): void => {
-        it(`initializeGrowthRate - should give the converted when it is greater than 1.0 '${currentGrowthRate}'`, (): void => {
+      async.each(
+        [
+          '5',
+          '2.0',
+          '1000',
+          '1.001',
+          '1.2',
+          '1.0000000001',
+          '1.09',
+          '7'
+        ], (currentGrowthRate: string): void => {
+          it(`initializeGrowthRate - should give the converted when it is greater than 1.0 '${currentGrowthRate}'`, (): void => {
           // Arrange
 
-          const parameters: Parameters = new Parameters(instance(consoleWrapper), instance(taskLibWrapper))
+            const parameters: Parameters = new Parameters(instance(consoleWrapper), instance(taskLibWrapper))
 
-          // Act
-          parameters.initialize('', currentGrowthRate, '', '', '') // > 1.0
+            // Act
+            parameters.initialize('', currentGrowthRate, '', '', '') // > 1.0
 
-          // Assert
-          expect(parameters.growthRate).to.equal(parseFloat(currentGrowthRate))
+            // Assert
+            expect(parameters.growthRate).to.equal(parseFloat(currentGrowthRate))
+          })
         })
-      })
-
-    /** initializeTestFactor */
-    async.each(
-      [
-        '',
-        ' ',
-        'abc',
-        '===',
-        '!2',
-        'null',
-        'undefined'
-      ], (currentTestFactor: string): void => {
-        it(`initializeTestFactor - should give a value of 1.5 when base size is invalid input '${currentTestFactor}'`, (): void => {
+    })
+    describe('initializeTestFactor', (): void => {
+      async.each(
+        [
+          '',
+          ' ',
+          'abc',
+          '===',
+          '!2',
+          'null',
+          'undefined'
+        ], (currentTestFactor: string): void => {
+          it(`initializeTestFactor - should give a value of 1.5 when base size is invalid input '${currentTestFactor}'`, (): void => {
           // Arrange
 
-          const parameters: Parameters = new Parameters(instance(consoleWrapper), instance(taskLibWrapper))
+            const parameters: Parameters = new Parameters(instance(consoleWrapper), instance(taskLibWrapper))
 
-          // Act
-          parameters.initialize('', '', currentTestFactor, '', '') // NaN
+            // Act
+            parameters.initialize('', '', currentTestFactor, '', '') // NaN
 
-          // Assert
-          expect(parameters.testFactor).to.equal(1.5)
+            // Assert
+            expect(parameters.testFactor).to.equal(1.5)
+          })
         })
-      })
 
-    async.each(
-      [
-        '0',
-        '-0.0000009',
-        '-2',
-        '-1.2',
-        '-5',
-        '-0.9999999999'
-      ], (currentTestFactor: string): void => {
-        it(`initializeTestFactor - should give a value of 1.5 when converted value is less than 0.0 '${currentTestFactor}'`, (): void => {
+      async.each(
+        [
+          '0',
+          '-0.0000009',
+          '-2',
+          '-1.2',
+          '-5',
+          '-0.9999999999'
+        ], (currentTestFactor: string): void => {
+          it(`initializeTestFactor - should give a value of 1.5 when converted value is less than 0.0 '${currentTestFactor}'`, (): void => {
           // Arrange
 
-          const parameters: Parameters = new Parameters(instance(consoleWrapper), instance(taskLibWrapper))
+            const parameters: Parameters = new Parameters(instance(consoleWrapper), instance(taskLibWrapper))
 
-          // Act
-          parameters.initialize('', '', currentTestFactor, '', '') // < 0.0
+            // Act
+            parameters.initialize('', '', currentTestFactor, '', '') // < 0.0
 
-          // Assert
-          expect(parameters.testFactor).to.equal(1.5)
+            // Assert
+            expect(parameters.testFactor).to.equal(1.5)
+          })
         })
-      })
 
-    async.each(
-      [
-        '5',
-        '2.0',
-        '1000',
-        '1.001',
-        '1.2',
-        '0.000000000000009',
-        '0.09',
-        '7'
-      ], (currentTestFactor: string): void => {
-        it(`initializeTestFactor - should give the converted when it is greater than 0.0 '${currentTestFactor}'`, (): void => {
+      async.each(
+        [
+          '5',
+          '2.0',
+          '1000',
+          '1.001',
+          '1.2',
+          '0.000000000000009',
+          '0.09',
+          '7'
+        ], (currentTestFactor: string): void => {
+          it(`initializeTestFactor - should give the converted when it is greater than 0.0 '${currentTestFactor}'`, (): void => {
           // Arrange
 
-          const parameters: Parameters = new Parameters(instance(consoleWrapper), instance(taskLibWrapper))
+            const parameters: Parameters = new Parameters(instance(consoleWrapper), instance(taskLibWrapper))
 
-          // Act
-          parameters.initialize('', '', currentTestFactor, '', '') // > 0.0
+            // Act
+            parameters.initialize('', '', currentTestFactor, '', '') // > 0.0
 
-          // Assert
-          expect(parameters.testFactor).to.equal(parseFloat(currentTestFactor))
+            // Assert
+            expect(parameters.testFactor).to.equal(parseFloat(currentTestFactor))
+          })
         })
-      })
-
-    /** initializeFileMatchingPatterns */
-    async.each(
-      [
-        '',
-        ' ',
-        '     ',
-        '\n'
-      ], (currentFileMatchingPatterns: string): void => {
-        it(`initializeFileMatchingPatterns - should give a value of [**/*] when input is invalid '${currentFileMatchingPatterns}'`, (): void => {
+    })
+    describe('initializeFileMatchingPatterns', (): void => {
+      async.each(
+        [
+          '',
+          ' ',
+          '     ',
+          '\n'
+        ], (currentFileMatchingPatterns: string): void => {
+          it(`initializeFileMatchingPatterns - should give a value of [**/*] when input is invalid '${currentFileMatchingPatterns}'`, (): void => {
           // Arrange
-          const expectedOutput: string[] = ['**/*']
-          const parameters: Parameters = new Parameters(instance(consoleWrapper), instance(taskLibWrapper))
+            const expectedOutput: string[] = ['**/*']
+            const parameters: Parameters = new Parameters(instance(consoleWrapper), instance(taskLibWrapper))
 
-          // Act
-          parameters.initialize('', '', '', currentFileMatchingPatterns, '')
+            // Act
+            parameters.initialize('', '', '', currentFileMatchingPatterns, '')
 
-          // Assert
-          expect(parameters.fileMatchingPatterns).to.deep.equal(expectedOutput)
+            // Assert
+            expect(parameters.fileMatchingPatterns).to.deep.equal(expectedOutput)
+          })
         })
-      })
 
-    async.each(
-      [
-        'abc',
-        'abc def hik',
-        '*.ada *.js *ts *.bb *txt'
-      ], (currentFileMatchingPatterns: string): void => {
-        it(`initializeFileMatchingPatterns - should not break the string up '${currentFileMatchingPatterns}'`, (): void => {
+      async.each(
+        [
+          'abc',
+          'abc def hik',
+          '*.ada *.js *ts *.bb *txt'
+        ], (currentFileMatchingPatterns: string): void => {
+          it(`initializeFileMatchingPatterns - should not break the string up '${currentFileMatchingPatterns}'`, (): void => {
           // Arrange
 
-          const parameters: Parameters = new Parameters(instance(consoleWrapper), instance(taskLibWrapper))
+            const parameters: Parameters = new Parameters(instance(consoleWrapper), instance(taskLibWrapper))
 
-          // Act
-          parameters.initialize('', '', '', currentFileMatchingPatterns, '')
+            // Act
+            parameters.initialize('', '', '', currentFileMatchingPatterns, '')
 
-          // Assert
-          expect(parameters.fileMatchingPatterns).to.deep.equal([currentFileMatchingPatterns])
+            // Assert
+            expect(parameters.fileMatchingPatterns).to.deep.equal([currentFileMatchingPatterns])
+          })
         })
-      })
 
-    async.each(
-      [
-        '*.ada\n*.js\n*ts\n*.bb\n*txt',
-        'abc\ndef\nhij'
-      ], (currentFileMatchingPatterns: string): void => {
-        it(`initializeFileMatchingPatterns - should break the string at the newline character '${currentFileMatchingPatterns}'`, (): void => {
+      async.each(
+        [
+          '*.ada\n*.js\n*.ts\n*.bb\n*.txt',
+          'abc\ndef\nhij'
+        ], (currentFileMatchingPatterns: string): void => {
+          it(`initializeFileMatchingPatterns - should break the string at the newline character '${currentFileMatchingPatterns}'`, (): void => {
           // Arrange
-          const expectedOutput: string[] = currentFileMatchingPatterns.split('\n')
-          const parameters: Parameters = new Parameters(instance(consoleWrapper), instance(taskLibWrapper))
+            const expectedOutput: string[] = currentFileMatchingPatterns.split('\n')
+            const parameters: Parameters = new Parameters(instance(consoleWrapper), instance(taskLibWrapper))
 
-          // Act
-          parameters.initialize('', '', '', currentFileMatchingPatterns, '')
+            // Act
+            parameters.initialize('', '', '', currentFileMatchingPatterns, '')
 
-          // Assert
-          expect(parameters.fileMatchingPatterns).to.deep.equal(expectedOutput)
+            // Assert
+            expect(parameters.fileMatchingPatterns).to.deep.equal(expectedOutput)
+          })
         })
-      })
+    })
 
-    /** initializeCodeFileExtensions */
-
-    // 115
-    async.each(
-      [
-        '',
-        ' ',
-        '     ',
-        '\n'
-      ], (currentCodeFileExtensions: string): void => {
-        it(`initializeCodeFileExtensions - should give the default array when input is invalid '${currentCodeFileExtensions}'`, (): void => {
+    describe('initializeCodeFileExtensions', (): void => {
+      async.each(
+        [
+          '',
+          ' ',
+          '     ',
+          '\n'
+        ], (currentCodeFileExtensions: string): void => {
+          it(`initializeCodeFileExtensions - should give the default array when input is invalid '${currentCodeFileExtensions}'`, (): void => {
           // Arrange
-          const parameters: Parameters = new Parameters(instance(consoleWrapper), instance(taskLibWrapper))
+            const parameters: Parameters = new Parameters(instance(consoleWrapper), instance(taskLibWrapper))
 
-          // Act
-          parameters.initialize('', '', '', '', currentCodeFileExtensions)
+            // Act
+            parameters.initialize('', '', '', '', currentCodeFileExtensions)
 
-          // Assert
-          expect(parameters.codeFileExtensions.length).to.equal(108)
+            // Assert
+            expect(parameters.codeFileExtensions.length).to.equal(108)
+          })
         })
-      })
+
+      async.each(
+        [
+          'ada\njs\nts\nbb\ntxt',
+          'abc\ndef\nhij',
+          'ts'
+        ], (currentFileMatchingPatterns: string): void => {
+          it(`initializeFileMatchingPatterns - should break the string at the newline character '${currentFileMatchingPatterns}'`, (): void => {
+          // Arrange
+            const expectedOutput: string[] = currentFileMatchingPatterns.split('\n')
+            expectedOutput.map(entry => `*.${entry}`)
+
+            const parameters: Parameters = new Parameters(instance(consoleWrapper), instance(taskLibWrapper))
+
+            // Act
+            parameters.initialize('', '', '', currentFileMatchingPatterns, '')
+
+            // Assert
+            expect(parameters.fileMatchingPatterns).to.deep.equal(expectedOutput)
+          })
+        })
+    })
   }) // end of describe
 }) // end of describe
