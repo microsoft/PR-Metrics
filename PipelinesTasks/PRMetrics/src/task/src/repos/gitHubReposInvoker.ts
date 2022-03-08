5464af63dd424e174c566d8f4cff31d8b2087b54
Muiris Woulfe <mwoulfe@microsoft.com>
2022-03-08 13:55:02 +0000

    Adding exception handling

--- PipelinesTasks/PRMetrics/src/task/src/repos/gitHubReposInvoker.ts
+++ PipelinesTasks/PRMetrics/src/task/src/repos/gitHubReposInvoker.ts
@@ -136,8 +136,20 @@ export default class GitHubReposInvoker extends BaseReposInvoker {
       }
 
       await this.invokeApiCall(async (): Promise<void> => {
-        const result: CreateReviewCommentResponse = await this._octokitWrapper.createReviewComment(this._owner!, this._repo!, this._pullRequestId!, content, fileName, this._commitId!)
-        this._logger.logDebug(JSON.stringify(result))
+        try {
+          const result: CreateReviewCommentResponse = await this._octokitWrapper.createReviewComment(this._owner!, this._repo!, this._pullRequestId!, content, fileName, this._commitId!)
+          this._logger.logDebug(JSON.stringify(result))
+        } catch (error: any) {
+          // A 422 HttpError may be thrown if the file is the set of file changes are too large for a diff to be displayed.
+          if (error.name === 'HttpError' && error.status === 422) {
+            const properties: string[] = Object.getOwnPropertyNames(error)
+            properties.forEach((property: string): void => {
+              this._logger.logDebug(`${error.name} – ${property}: ${JSON.stringify(error[property])}`)
+            })
+          } else {
+            throw error
+          }
+        }
       })
     } else {
       await this.invokeApiCall(async (): Promise<void> => {
