f03c1de081d6aec04c23f06255b0f369c14c8a6b
Muiris Woulfe <mwoulfe@microsoft.com>
2022-11-04 11:57:17 +0000

    Removing test logic

--- src/task/src/wrappers/octokitWrapper.ts
+++ src/task/src/wrappers/octokitWrapper.ts
@@ -191,7 +191,7 @@ export default class OctokitWrapper {
     for (let i: number = 0; i < parsableDiffResponses.length && line === -1; i++) {
       const diffParsed: GitDiff = parseGitDiff(parsableDiffResponses[i]!)
       if (diffParsed.files.length !== 1) {
-        throw Error(diffParsed.files.length + ' files were located instead of the expected 1.' + i + ' ' + parsableDiffResponses[i]!)
+        throw Error(diffParsed.files.length + ' files were located instead of the expected 1.')
       }
 
       diffParsed.files.forEach((file: AnyFileChange): void => {
