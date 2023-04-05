c207f96749814054c0ab3cc0e7eb7a1d00d23bf3
Muiris Woulfe <mwoulfe@microsoft.com>
2023-04-05 19:04:27 +0100

    XS⚠️ ◾ Release v1.5.1 (#369)
    Creating release v1.5.1, which is a quarterly release comprising package updates.
    
    There are three additional minor changes being made as part of this release:
    - A pipeline improvement to ensure automatic releases proceed as expected next quarter, without the permissions issue seen this time.
    - Updated comments to be placed within the release PRs, to better clarify the manual actions that need to be undertaken.
    - Updating documentation and Azure DevOps pipelines to reflect changes made to Azure DevOps defaults in September 2022 that customers may encounter.

--- src/task/src/repos/gitHubReposInvoker.ts
+++ src/task/src/repos/gitHubReposInvoker.ts
@@ -192,7 +192,7 @@ export default class GitHubReposInvoker extends BaseReposInvoker {
 
     const options: OctokitOptions = {
       auth: process.env.PR_METRICS_ACCESS_TOKEN,
-      userAgent: 'PRMetrics/v1.5.0',
+      userAgent: 'PRMetrics/v1.5.1',
       log: {
         debug: (message: string): void => this._logger.logDebug(`Octokit – ${message}`),
         info: (message: string): void => this._logger.logInfo(`Octokit – ${message}`),
