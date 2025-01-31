21688c24bf8d70fdd4b745b11349341823e8484b
Muiris Woulfe <mwoulfe@microsoft.com>
2025-01-31 17:03:27 +0000

    Adding polyfill

--- src/task/index.ts
+++ src/task/index.ts
@@ -3,10 +3,20 @@
  * Licensed under the MIT License.
  */
 
+/**
+ * This ESM module polyfills "require".
+ * It is needed e.g. when bundling ESM scripts via "@vercel/ncc" because of https://github.com/vercel/ncc/issues/791.
+ */
+
 import "reflect-metadata";
 import PullRequestMetrics from "./src/pullRequestMetrics.js";
 import { container } from "tsyringe";
+import { createRequire } from 'node:module';
 import { exitCodeForFailure } from "./src/utilities/constants.js";
+import url from 'node:url';
+
+const filename = url.fileURLToPath(import.meta.url);
+globalThis.require = createRequire(filename);
 
 const run = async (): Promise<void> => {
   const pullRequestMetrics: PullRequestMetrics =
