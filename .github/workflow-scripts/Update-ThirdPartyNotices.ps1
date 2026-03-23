f2c0c7161f45a6c791127409a47df77db5c35112
Muiris Woulfe <mw.github@outlook.com>
2026-03-23 17:20:53 +0000

    build(deps): add generate-license-file and improve license script
    - Add generate-license-file@4.1.1 as a dev dependency
    - Rewrite Update-ThirdPartyNotices.ps1 to dynamically detect and
      strip header/footer markers instead of using hardcoded line offsets
    - Fix malformed XML in PoliCheckExclusions.xml (remove nested tags,
      normalize indentation)

--- .github/workflow-scripts/Update-ThirdPartyNotices.ps1
+++ .github/workflow-scripts/Update-ThirdPartyNotices.ps1
@@ -3,10 +3,41 @@
 
 $inputFile = './third-party-licenses.txt'
 $outputFile = 'src/LICENSE.txt'
+$marker = 'This file was generated with generate-license-file'
 
 $lines = Get-Content -Path $inputFile
 $lines = $lines | ForEach-Object { $_.TrimEnd() }
-$lines = $lines[2..($lines.Count - 3)]
+
+# Remove header (marker line + URL line).
+$start = 0
+if ($lines[$start].Contains($marker))
+{
+    $start++
+}
+
+if ($start -lt $lines.Count -and $lines[$start].StartsWith('https://www.npmjs.com/'))
+{
+    $start++
+}
+
+# Remove footer (separator + marker line + URL line).
+$end = $lines.Count - 1
+if ($lines[$end].StartsWith('https://www.npmjs.com/'))
+{
+    $end--
+}
+
+if ($end -ge $start -and $lines[$end].Contains($marker))
+{
+    $end--
+}
+
+if ($end -ge $start -and $lines[$end] -match '^-+$')
+{
+    $end--
+}
+
+$lines = $lines[$start..$end]
 
 $content = ($lines -join "`n").TrimEnd() + "`n"
 $outputPath = Join-Path -Path $PWD -ChildPath $outputFile
