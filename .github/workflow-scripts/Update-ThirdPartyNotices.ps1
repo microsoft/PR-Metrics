305d9ee5c1d355fbd95856a98d8f94176f2f8120
Muiris Woulfe <mw.github@outlook.com>
2026-03-23 15:09:39 +0000

    refactor(licenses): simplify license update logic - Removed unnecessary tagline handling from script - Streamlined the process of updating third-party notices

--- .github/workflow-scripts/Update-ThirdPartyNotices.ps1
+++ .github/workflow-scripts/Update-ThirdPartyNotices.ps1
@@ -6,28 +6,7 @@ $outputFile = 'src/LICENSE.txt'
 
 $lines = Get-Content -Path $inputFile
 $lines = $lines | ForEach-Object { $_.TrimEnd() }
-
-$taglinePattern = 'This file was generated with generate-license-file!'
-$taglineIndex = -1
-for ($i = $lines.Count - 1; $i -ge 0; $i--)
-{
-    if ($lines[$i].StartsWith($taglinePattern))
-    {
-        $taglineIndex = $i
-        break
-    }
-}
-
-if ($taglineIndex -ge 0)
-{
-    $end = $taglineIndex - 1
-    if ($end -ge 0 -and $lines[$end] -match '^-+$')
-    {
-        $end--
-    }
-
-    $lines = $lines[0..$end]
-}
+$lines = $lines[2..($lines.Count - 3)]
 
 $content = ($lines -join "`n").TrimEnd() + "`n"
 $outputPath = Join-Path -Path $PWD -ChildPath $outputFile
