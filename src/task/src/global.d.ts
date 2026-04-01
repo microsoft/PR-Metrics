d5a6d280a7d50c35fcdc228ad1d75efec7bcfe9d
Muiris Woulfe <mw.github@outlook.com>
2026-04-01 20:03:39 +0100

    Use Error.isError() for safer error handling (Node 24)

---
+++ src/task/src/global.d.ts
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) Microsoft Corporation.
+ * Licensed under the MIT License.
+ */
+
+/**
+ * Augments the built-in `ErrorConstructor` with `isError()` from Node 24.
+ * This can be removed once TypeScript ships native `Error.isError()` typings.
+ */
+interface ErrorConstructor {
+  isError(value: unknown): value is Error;
+}
