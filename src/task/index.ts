9c8f6770de36d206defc84dfcb6d584563c53d50
Muiris Woulfe <mw.github@outlook.com>
2026-06-16 14:44:08 +0100

    ci: revert to detection-based SDL ESLint; temp .ts touch to validate

--- src/task/index.ts
+++ src/task/index.ts
@@ -3,6 +3,7 @@
  * Licensed under the MIT License.
  */
 
+// Temporary touch to arm the SDL ESLint scan for validation; remove before merge.
 import createPullRequestMetrics from "./src/compositionRoot.js";
 import { exitCodeForFailure } from "./src/utilities/constants.js";
 
